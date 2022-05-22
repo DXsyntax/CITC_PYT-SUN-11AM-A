a443c64fde63e6846a37917076620dc5678c91cc
Dexter <54725203+DXsyntax@users.noreply.github.com>
2022-05-22 13:42:49 +0800

    uploading remaining L5 exe
    in drawing different shapes

---
+++ L5/L5_shapes.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+pen = Turtle()
+# descriptive variable name
+
+# TRIANGLE
+# triangle = 3
+# for c in range(triangle):
+#     pen.forward(90)
+#     pen.right(120)
+
+# DECAGON 
+# decagon = 10
+# for c in range(decagon):
+#     pen.forward(60)
+#     pen.right(36)
+
+# FIVE POINT STAR
+# star = 5
+
+# for c in range(star):
+#   pen.forward(60)
+#   pen.right(144)
